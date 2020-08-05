fc497eedb7440b7c5f1abfd819f4a90374878d91
Mikel1991 <mikel362d@gmail.com>
2020-08-05 13:01:02 -0700

    added db.js to handle offline request with indexDB. Functionality achieved

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
+  "/db.js",
   "/app.js",
   "/index.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 const CACHE_NAME = "static-cache-v2";
